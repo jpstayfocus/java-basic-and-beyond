f4a992122c607d5ceda75f2dc62b59767e51473a
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 18:37:03 -0500

    Create arithmetic.java

---
+++ the basics/arithmetic.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOne =  10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+        // BODMAS
+        // B- Brackets
+        // O- Orders (powers/indices or roots)
+        // D- Division
+        // M- Multiplication
+        // A- Addition
+        // S- Subtraction
+        System.out.println((2+2) * (3/1) * 2);
+    }
+}
