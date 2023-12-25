a1b2a72b1c5271ef08c07075fd2bd6634d3bf9a8
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 18:30:20 -0500

    Create numeric-literals.java

---
+++ the basics/numeric-literals.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1_000_000_008; // represent big numbers with underscore for readability
+        System.out.println(amount);
+    }
+}
