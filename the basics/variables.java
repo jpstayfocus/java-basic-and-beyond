e148bb86d66a340eb88f9f3258bc5f2284ee402b
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 18:06:35 -0500

    Create variables.java

---
+++ the basics/variables.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Primitives
+        int number1 = 20;   // store integer
+        int number2 = 20;
+        int result = number1 + number2;
+        System.out.println(result);
+        double pi = 3.14;   // store decimal
+        boolean isAdult = false;    // store boolean
+        char a = 'A';  // store character
+    }
+}
