fffa64938239972f2dada5fe3a1c0687b5ec2775
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 20:56:04 -0500

    Create increment-decrement-operator.java

---
+++ the basics/increment-decrement-operator.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+         /* decrement operator --
+        int number = 0;
+        System.out.println(number--);
+        System.out.println(number);
+        int numberTwo = 0;
+        System.out.println(--numberTwo);
+        System.out.println(numberTwo);
+        */
+
+        /* Increment operator ++
+        int number = 0;
+        System.out.println(number++);
+        System.out.println(number);
+        int numberTwo = 0;
+        System.out.println(++numberTwo);
+        System.out.println(numberTwo);
+        */
+        
+        
+        int increment = number + 1;
+        int decrement = number - 1;
+        System.out.println(increment);
+        System.out.println(decrement);
+    }
+}
