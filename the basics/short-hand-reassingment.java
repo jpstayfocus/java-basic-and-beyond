ff7070a62669395b765af6721ed0f8c31d0b11ff
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 21:10:22 -0500

    Create short-hand-reassingment.java

---
+++ the basics/short-hand-reassingment.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOne = 2;
+        int numberTwo = 10;
+        numberTwo += numberOne;
+        System.out.println(numberTwo);
+        
+        /*
+        x += y is x = x + y
+        x -= y is x = x - y
+        x *= y is x = x * y
+        x /= y is x = x / y
+        x %= y is x = x % y
+        x ^= y is x = x ^ y
+        */
+
+        /*
+        int numberOne = 2;
+        int numberTwo = 10;
+        int result = numberTwo + 2;
+        System.out.println(result);
+        */
+    }
+}
