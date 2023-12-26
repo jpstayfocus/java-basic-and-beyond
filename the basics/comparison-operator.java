421ee0d0db6c54f721faaca4b112b77458ef0bd8
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 21:01:07 -0500

    Create comparison-operator.java

---
+++ the basics/comparison-operator.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // comparison Operators
+        // <, <=, >, >=, ==, !=
+        System.out.println(10 < 10);
+        System.out.println(10 > 10);
+        System.out.println(10 <= 10);
+        System.out.println(10 <= 10);
+        System.out.println(10 == 10);
+        System.out.println(10 != 10);
+
+        System.out.println();
+
+        boolean isAdult = 15 >= 16;
+        System.out.println(isAdult);
+
+    }
+}
