7a584fde9d70f8396c8389c6b26f6e39d88d9b27
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 21:22:43 -0500

    Create string.java

---
+++ the basics/string.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // strings - allows to store a sequence of character
+        String brand = "jpstayfocus";
+        String variableTwo = " Learning Java";
+        String addString = brand + variableTwo;
+        System.out.println(addString);
+        System.out.println(addString.toLowerCase());
+        System.out.println(addString.toUpperCase());
+        System.out.println(addString.substring(0, 5));
+
+
+    }
+}
