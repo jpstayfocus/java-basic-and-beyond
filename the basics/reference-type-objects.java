a10ec897d252ca10bdc4248a2d7aab9122e4a522
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-25 21:35:39 -0500

    Update reference-type-objects.java

--- the basics/reference-type-objects.java
+++ the basics/reference-type-objects.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // reference type / objects
         int age = 21; // primitive
-        Point pointA = new Point(10, 10); // reference type allows to store more complex value
+        Point pointA = new Point(10, 10); // reference type allows to store more complex value / pointA is an object
         System.out.println("pointA = " + pointA);
         pointA.move(10, 11);
         System.out.println("pointA = " + pointA);
