ab95719f1a7e4068bbf5e0aa5d3aaeee6fc71083
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-27 00:16:22 -0500

    Update for-loops.java

--- loops/for-loops.java
+++ loops/for-loops.java
@@ -1,1 +1,19 @@
+import java.awt.*;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        // for loops
+        System.out.println("Start of for loop");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hello " + i);
+        }
+        System.out.println("End of for loop");
+
+        System.out.println("*******************");
+        System.out.println("reverse for loop");
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Hello " + i);
+        }
+    }
+}
