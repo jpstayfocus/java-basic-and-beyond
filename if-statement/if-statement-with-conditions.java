4af143e5a6bf5e02dd2ffc35b6b1c2ebfe58824a
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-27 09:50:26 -0500

    Create if-statement-with-conditions.java

---
+++ if-statement/if-statement-with-conditions.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    // if statement
+        int age = 17;
+        boolean isAdult = age >= 16;
+
+        if (isAdult) {
+            System.out.println("is adult :-)");
+        } else{
+            System.out.println("is not adult :-(");
+        }
+
+    }
+}
