d6e9b2faa90c2bbb59095b13492bcc0fb854428e
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-27 09:18:40 -0500

    Update and rename if-statement to if-statement.java

---
+++ if-statement/if-statement.java
@@ -0,0 +1,17 @@
+
+
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    // if statement
+        if (true) {
+            System.out.println("this code runs when true");
+        } else{
+            System.out.println("this code runs when false");
+        }
+        System.out.println("code outside the if statement");
+
+    }
+}
