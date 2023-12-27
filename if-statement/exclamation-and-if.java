48932fa56a1b373896426bb7de8b2b7bea87ba3b
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-27 13:42:47 -0500

    Create exclamation-and-if.java

---
+++ if-statement/exclamation-and-if.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(!true);
+        System.out.println(!false);    
+    boolean isAdult =  false;
+    if (!isAdult) {
+        System.out.println("not an Adult -:)");
+    }
+
+    }
+}
