4dff9b87b727249c76e19cde4fdd3a27afd1a3e3
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-27 00:46:00 -0500

    Create while-loop.java

---
+++ loops/while-loop.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    // while loops
+        int i = 5;
+        boolean keepLooping = true;
+        while (keepLooping){
+            System.out.println("hello " + i++ );
+            keepLooping = false;
+        }
+
+
+    }
+}
