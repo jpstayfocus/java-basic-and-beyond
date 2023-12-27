a52096af1f54fcef84efd1ce94d8e20bafa24ebd
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-12-27 00:28:29 -0500

    Create loop-and-array.java

---
+++ loops/loop-and-array.java
@@ -0,0 +1,25 @@
+
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        //  loops and arrays
+        String[] names = {"Jones", "Peter", "Jacon", "Maraket", "Mikah"};
+
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+
+        /*
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
+        System.out.println();
+        System.out.println("Length of the array is " + names.length);
+        */
+
+    }
+}
