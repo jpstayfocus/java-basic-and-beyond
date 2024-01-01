f854f5896a5a5f6aa2cea9ae30ec8182cccd8c20
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-01-01 18:39:37 -0500

    Create methods.java

---
+++ methos/methods.java
@@ -0,0 +1,15 @@
+public class Main {
+    /*
+    When building method:
+    - Access modifiers
+    - Optional static
+    - Return type
+    - Name
+    - Optional parameters
+    - Method Body
+    - Optional return value
+    */
+    public static void main(String[] args) {
+
+    }
+}
