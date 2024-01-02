0ba7de18b3f548cdb495218882f65dc87afda99e
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-01-02 10:29:57 -0500

    Update enums-methods.java

--- beyond-basics/enums-methods.java
+++ beyond-basics/enums-methods.java
@@ -14,3 +14,23 @@ public class Main {
         System.out.println(GenderConstants.MALE); // String
     }
 }
+
+// create a class with emum
+
+public enum Gender {
+      // Gender
+        MALE,
+        FEMALE
+}
+
+public enum TShirtSize {
+    S,
+    M,
+    L,
+    XL
+}
+
+public class GenderConstants {
+    public static final String MALE = "MALE";
+    public static final String FEMALE = "FEMALE";
+}
