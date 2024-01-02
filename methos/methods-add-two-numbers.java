de134a97d14ae2ac626d5a9bd704728972b62b09
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-01-01 19:14:36 -0500

    Update methods-add-two-numbers.java

--- methos/methods-add-two-numbers.java
+++ methos/methods-add-two-numbers.java
@@ -1,3 +1,8 @@
+package com.jpstayfocus;
+
+import java.awt.*;
+import java.util.Arrays;
+
 public class Main {
             /*
             When building method:
@@ -12,17 +17,33 @@ public class Main {
 
             // methods that adds 2 numbers
 
-            private static int add(int n1, int n2){
+            private static int add(int n1, int n2) {
               int result = n1 + n2;
               return result;
             }
 
+            private static void printBrand() { // no parameters
+                 System.out.println("jpstayfocus");
+            }
+
+            private static void isAdult(int age){  // naming convention example: isAdult
+                if (age >= 16){
+                    System.out.println("adult :) ");
+                } else{
+                    System.out.println("not an adult :-(");
+                }
+            }
 
             public static void main(String[] args) {
                 int result = add(10, 10);
                 int result2 = add(10, 100);
                 System.out.println(result);
                 System.out.println(result2);
+                printBrand();
+                isAdult(16);
+                isAdult(15);
+
+
             }
 
 }
