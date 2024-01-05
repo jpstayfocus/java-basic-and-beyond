df904be8b39d5a165f929efd9da6bdf8ef7c6be3
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-01-04 21:07:31 -0500

    Update string.java

--- the basics/string.java
+++ the basics/string.java
@@ -9,6 +9,24 @@ public class Main {
         System.out.println(addString.toUpperCase());
         System.out.println(addString.substring(0, 5));
 
+        /*
+        public class Main {
+
+    public static void main(String[] args) {
+        // Strings
+        String name = "Jamila";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(false);
+        System.out.println("Hello   ".trim());
+        System.out.println(name.startsWith("J"));
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1, 4));
+        System.out.println(name.replace("J", "S"));
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        */
+
 
     }
 }
