e1908039da8f7aed4d3dd7fa2a57705b6dd802c3
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-01-07 00:11:22 -0500

    Create problem-with-double.java

---
+++ big-decimal/problem-with-double.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // problem with double
+
+        double number1 =  0.02;
+        double number2 =  0.03;
+        double result = number2 - number1;
+        System.out.println(result); // precision: 0.009999999999999998
+        
+    }}
