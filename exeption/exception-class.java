7a64555110dd1a09266a79350f24ebb10593a0d1
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2024-01-07 11:15:43 -0500

    Create exception-class.java

---
+++ exeption/exception-class.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Exception
+
+        try{
+            int number  = Integer.parseInt("1"); // if you add 1x the first catch will be executed
+            System.out.println(number);
+
+            for ( int i=10; i>=0; i--){
+                System.out.println(10 / i);
+            }
+
+            // we can combine two exceptions into one
+        } catch (NumberFormatException | ArithmeticException e){ // since e is an object, then
+            System.out.println(e.getMessage());
+            // you can print the stack Trace
+            e.printStackTrace();
+
+            // if you dont know the exception type
+        } catch (Exception e){
+            // catch all exception
+        }
+    }
+}
